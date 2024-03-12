adbfc53d5c53bf1382dae58a5a1e681e086e5e25
mkiuzzal01 <mkiuzzal07@gmail.com>
2024-03-12 22:52:25 +0600

    this project ready for development

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className='text-2xl font-extrabold'></h1>
+      
+    </>
+  )
+}
+
+export default App
